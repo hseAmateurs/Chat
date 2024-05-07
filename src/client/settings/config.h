de99fb73a5862b01ea5a99eb53d556ccdb905169
Shon Qwezon <shonzbso@gmail.com>
2024-05-07 11:48:18 +0300

    Init config

---
+++ src/client/settings/config.h
@@ -0,0 +1,16 @@
+//
+// Created by Shon on 06.05.2024.
+//
+
+#ifndef CHAT_CONFIG_H
+#define CHAT_CONFIG_H
+
+#include <QString>
+
+namespace cfg {
+    namespace database {
+        const QString FILENAME = "cache.sqlite3";
+    }
+}
+
+#endif //CHAT_CONFIG_H
